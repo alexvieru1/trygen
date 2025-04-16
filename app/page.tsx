ab20991b0854a791b1ca07ff3a4c70fc61e0f0da
Alexandru Vieru <vieru.alexandruu@gmail.com>
2025-04-17 00:05:54 +0300

    added lenis

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { HeroParallax } from "@/components/ui/hero-parallax";
 export default function Home() {
   return (
     <div className="h-[1000px]">
-      <HeroParallax/>
-      <Footer/>
+      <HeroParallax />
+      <Footer />
     </div>
   );
 }
